dd6692f9fe0018fe6f64afe0ec720ec633238182
chmlsud <1188@holbertonschool.com>
2020-01-08 01:56:36 +0000

    Change the ID holberton

--- level_0/0.py
+++ level_0/0.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import requests
-input = {'id': 3453452, 'holdthedoor': 'Submit'}
+input = {'id': 1188, 'holdthedoor': 'Submit'}
 URL = 'http://158.69.76.135/level0.php'
 for var1 in range (0, 1024):
     requests.post(URL, input)
-
-print("Votos totales:{}".format(var1))
+print("{}".format(var1))
