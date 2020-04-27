7876929c9c3064ceb36a005a1768fed195e0e7aa
chmlsud <1188@holbertonschool.com>
2020-04-27 14:15:30 +0000

    Erase the files of the proyec

--- level_0/0.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import requests
-input = {'id': 1188, 'holdthedoor': 'Submit'}
-URL = 'http://158.69.76.135/level0.php'
-for var1 in range (0, 1024):
-    requests.post(URL, input)
-print("{}".format(var1))
